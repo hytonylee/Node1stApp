251c31dba8cc18578047e43d05e25eaa42c48fce
Tony Lee <hy.tony.lee@gmail.com>
2018-12-24 16:46:32 -0800

    Understanding build in Memory function in node

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const os = require('os');
 
 const totalMemory = os.totalmem()
-const freeMemory = os.freemen()
+const freeMemory = os.freemem()
 
-console.log('Total Memory: ' + totalMemory);
+// console.log('Total Memory: ' + totalMemory);
+
+// Template string
+// ES6 method
+
+console.log(` Total Memory ${totalMemory}.`)
+console.log(` Free Memory ${freeMemory}.`)
