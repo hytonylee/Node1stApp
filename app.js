129314c9210169646b45dee5b044dd5f901140b5
Tony Lee <hy.tony.lee@gmail.com>
2018-12-25 17:50:30 -0800

    Learning Node Event method

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const fs = require('fs');
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
-// const files = fs.readdirSync('./')
+emitter.on('messageLogged', function () {
+    console.log('Listener called');
+});
 
-// console.log(files);
+emitter.emit('messageLogged');
 
-fs.readdir('./', function (err, files) {
-    if (err) console.log('Error', err)
-    else console.log('Result', files)
-})
